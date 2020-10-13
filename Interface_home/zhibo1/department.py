df19679bb70eb048a1dd5e9c79bca4a4a829f353
yingTwo <zhyingNew2015@163.com>
2020-10-13 16:01:42 +0800

    修改变量书写

--- Interface_home/zhibo1/department.py
+++ Interface_home/zhibo1/department.py
@@ -1,7 +1,7 @@
 import requests
 
 corpid = 'ww228382814e46e3a7'
-corpsecret = 'iton7ufNa898V0By7pZ8rmUpW0nPcXDSBIVc3BA3H-k'#注释
+corpsecret = 'iton7ufNa898V0By7pZ8rmUpW0nPcXDSBIVc3BA3H-k'
 
 def get_token():
     url = f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}'
@@ -13,19 +13,19 @@ def test_addD():
     token = get_token()
     url = f'https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token={token}'
     data = {
-        "name": "HNTC4",
-        "name_en": "河南测试中心4",   #在web端显示的部门名称是此字段，是否接口文档有些问题
+        "name": "HNTC5",
+        "name_en": "河南测试中心5",   #在web端显示的部门名称是此字段，是否接口文档有些问题
         "parentid": 1,
-        "order": 9,
-        "id": 10
+        "order": 10,
+        "id": 11
     }
     d_new = requests.post(url, json=data)
     print(d_new.json())
     assert d_new.json()['errcode'] == 0
 
 def test_read():
-    Did = 1
-    url = f'https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={get_token()}&id={Did}'
+    d_id = 1
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={get_token()}&id={d_id}'
     print(requests.get(url).json())
     assert requests.get(url).json()['errcode'] == 0
 
@@ -39,8 +39,8 @@ def test_modify():
     assert requests.post(url, json=data).json()['errcode'] == 0
 
 def test_del():
-    Did = 10
-    url = f'https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={get_token()}&id={Did}'
+    d_id = 11
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={get_token()}&id={d_id}'
     D_del = requests.get(url).json()
     print(D_del)
     assert D_del['errcode'] == 0
